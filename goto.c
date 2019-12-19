44c37e51da4ff7bdfbde51dd69c43db45dbdd9e3
PADMASREE1999 <57174812+PADMASREE1999@users.noreply.github.com>
2019-12-19 12:05:05 +0530

    Add files via upload

---
+++ goto.c
@@ -0,0 +1,12 @@
+//goto example
+#include<stdio.h>
+int main()
+{
+	printf("\n gitam university");
+	goto a;
+	printf("\n dhyanahitha professional of studies");
+	printf("\n promizeit services pvt ltd");
+	a:
+		printf("\n hyderabad");
+	return 0;	
+}
