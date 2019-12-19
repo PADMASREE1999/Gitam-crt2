19401ffeccde38dd918c25e4778aa4e41862ff40
PADMASREE1999 <57174812+PADMASREE1999@users.noreply.github.com>
2019-12-19 09:24:10 +0530

    Add files via upload

---
+++ leapyear.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int year;
+	scanf("%d",&year);
+	
+ (year%4==0 && year%100!=0) ? printf("LEAP YEAR") :
+        (year%400 ==0 ) ? printf("LEAP YEAR") : printf("NOT A LEAP YEAR");
+       return 0; 
+    }
