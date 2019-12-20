5b2e8ad0750dc52f5b3865f7eacb8998240aedd0
PADMASREE1999 <57174812+PADMASREE1999@users.noreply.github.com>
2019-12-20 10:24:46 +0530

    Add files via upload

---
+++ increment.c
@@ -0,0 +1,14 @@
+//increment and decrement
+#include<stdio.h>
+int main()
+{
+	int a=10,b=10;
+	printf("%d",a);
+	printf("%d",a++);
+	printf("%d",a);
+	printf("\n%d",b);
+	printf("%d",++b);
+	printf("%d",b);
+	return 0;
+	
+}
